33ab14e3624ace89d48c35673c2e1fbfe3672b1d
Arlen Asael <asaelmarencojaime5867@gmail.com>
2022-11-12 05:04:34 -0600

    Subiendo app completa

---
+++ exams/venous.js
@@ -0,0 +1,123 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+export default function venous() {
+    return (
+
+        <View>
+            <View style={styles.container}>
+                <View style={styles.arrow}>
+                    <AntDesign name="caretleft" size={40} color="black" />
+                </View>
+            </View>
+            //utilizamos la libreria de AntDesign para conseguir el icono de una flecha
+            posteriormente modificamos su tamaño en size
+
+            <View style={styles.boxElements}>
+                <View style={styles.boxText}>
+                    <Text style={styles.text1}>
+                        LLENADO VENOSO
+                    </Text>
+                </View>
+
+            //creamos un contenedor dentro de otro para guarda el estilo y el texto
+
+                <TouchableOpacity style={styles.stycolor}>
+                    <Text style={styles.examText}>
+                        MENOR A 20 SEGUNDOS
+                    </Text>
+                </TouchableOpacity>
+
+            //creamos otro contenerdor en el mismo y usamos TouchableOpacity, para
+            cuando el usuario toque una opcion esta haga una pequeña animación de 
+            opacidad y seguimos estos mismos pasos hasta llegar a copyright
+
+                <TouchableOpacity style={styles.stycolor}>
+                    <Text style={styles.examText}>
+                        MAYOR A 20 SEGUNDOS
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.copyright}>
+                    <AntDesign name="copyright" size={18} color="black" />
+                    <Text>
+                        Copyright FreedomCode
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+
+
+    )
+}
+
+const styles = StyleSheet.create({
+    stycolor: {
+        backgroundColor: '#03FFC6',
+        top: '5%',
+        height: '8%',
+        width: '70%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        margin: '3%',
+        borderRadius: 50,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        elevation: 8,
+
+    },
+    copyright: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '45%',
+        top: '90%',
+    },
+    container: {
+        top: '5%',
+        marginBottom: '5%'
+    },
+    arrow: {
+        left: '5%',
+        top: '5%',
+    },
+    boxElements: {
+        backgroundColor: '#fff',
+        height: '100%',
+        marginTop: '25%',
+        alignItems: 'center',
+        borderTopStartRadius: 40,
+        borderTopEndRadius: 40,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        elevation: 8,
+    },
+    box2: {
+        alignItems: 'center',
+        height: '100%',
+    },
+    boxText: {
+        alignItems: 'center',
+        top: '4%',
+
+
+    },
+    text1: {
+        fontSize: 25,
+    },
+
+    examText: {
+        fontSize: 25,
+    }
+
+});
